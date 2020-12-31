6ae49f061816e1e606bf04f1aee1febf1301ea12
Rob Peschke <rspesc31@gmail.com>
2020-12-31 10:35:52 -0500

    final product

---
+++ app/src/main/java/com/android/rollthebones/MainActivity.java
@@ -0,0 +1,50 @@
+package com.android.rollthebones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.util.Log;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton;
+
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        final ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+        final int[] diceArray = {R.drawable.dice1, R.drawable.dice2,R.drawable.dice3,R.drawable.dice4,R.drawable.dice5, R.drawable.dice6};
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("RollTheBones", "Button pressed");
+
+                Random randomNumberGenerator = new Random();
+
+                int leftDiceNumber = randomNumberGenerator.nextInt(6);
+                int rightDiceNumber = randomNumberGenerator.nextInt(6);
+
+
+                Log.d("RollTheBones", "The Random number is:" + leftDiceNumber);
+
+                leftDice.setImageResource(diceArray[leftDiceNumber]);
+
+                rightDice.setImageResource(diceArray[rightDiceNumber]);
+            }
+        });
+
+    }
+}
